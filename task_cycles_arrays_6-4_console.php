f963d927b650fca39cd1c4248d131da5e1909207
Yaroslav Yagodarov <ya.yagodarov@gmail.com>
2020-07-21 23:26:31 +0400

    fix task_cycles_arrays_6-4_console.php

--- task_cycles_arrays_6-4_console.php
+++ task_cycles_arrays_6-4_console.php
@@ -1,6 +1,5 @@
 <?php
-//    const INPUT_STRING = "Ведомость расходов АМОУ ДОД ДШИ-2 за Октябрь 2022";
-    const INPUT_STRING = "а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я";
+    const INPUT_STRING = "Ведомость расходов АМОУ ДОД ДШИ-2 за Октябрь 2022";
 
     $translitTable = array(
         "a" => "a",
@@ -32,60 +31,14 @@
         "щ" => "shch",
         "ъ" => "ie",
         "ы" => "y",
-        "ь" => " ",
+        "ь" => "",
         "э" => "e",
         "ю" => "yu",
-        "я" => "ya",
-
-/*        "А" => "А",
-        "Б" => "B",
-        "В" => "V",
-        "Г" => "G",
-        "Д" => "D",
-        "Е" => "E",
-        "Ё" => "YO",
-        "Ж" => "ZH",
-        "З" => "Z",
-        "И" => "I",
-        "Й" => "J",
-        "К" => "K",
-        "Л" => "L",
-        "М" => "M",
-        "Н" => "N",
-        "О" => "O",
-        "П" => "P",
-        "Р" => "R",
-        "С" => "S",
-        "Т" => "T",
-        "У" => "U",
-        "Ф" => "F",
-        "Х" => "KH",
-        "Ц" => "TS",
-        "Ч" => "CH",
-        "Ш" => "SH",
-        "Щ" => "SHCH",
-        "Ъ" => "IE",
-        "Ы" => "Y",
-        "Ь" => "",
-        "Э" => "E",
-        "Ю" => "YU",
-        "Я" => "YA",*/
+        "я" => "ya"
     );
 
-
     function translitRusEng($rusStr, $table) {
-//        $rusStr = mb_strtolower($rusStr);
-        print_r($rusStr."\n");
-        foreach (array_keys($table) as $value) {
-            echo $value . " ";
-        }
-        echo "\n";
-        foreach (array_values($table) as $value) {
-            echo $value . " ";
-        }
-        echo "\n";
-
-        return str_ireplace(array_keys($table), array_values($table), $rusStr);
+        return str_replace(array_keys($table), array_values($table), mb_strtolower($rusStr));
     }
 
     $rusInputString = INPUT_STRING;
